9779030ae94f42c32bf101be604e938882ff2833
NicolasFG <40153492+NicolasFG@users.noreply.github.com>
2020-10-15 00:05:55 -0500

    Update Bucket.h

--- ProyectoBD/Bucket.h
+++ ProyectoBD/Bucket.h
@@ -7,8 +7,6 @@ class Bucket {
 
 private:
 
-//Cada Bucket tiene solo un archivo.
-    //FixedRecord registros;
 
     int ProfundidadLocal;
 
@@ -18,24 +16,22 @@ private:
 
     int NumeroBucket;
 
-    long next_bucket;
-
     map<int, string> valores;
-
-
-
 
 public:
 
-//Inicio cada Bucket con su profundidad local y su tamaño
     Bucket() {
         this->ProfundidadLocal = 1;
         this->tamanio = 0;
-        this->NumeroBucket = 0;
+
+        srand(time(NULL));
+
+        int   num=1+rand()%(1001-1);;
+
+        this->NumeroBucket = num;
         CrearBucket();
     };
 
-//Funcion para crear el Bucket
    void CrearBucket(){
 
     fstream BucketFile;
@@ -47,19 +43,19 @@ public:
     this->BucketFileName = "Bucket" + ConverNumeroBucket + ".dat";
 
 
-    BucketFile.open(this->BucketFileName, ios::binary | ios::in | ios::out);
-
-    NumeroBucket++;
+    BucketFile.open(this->BucketFileName, ios::binary | ios::app);
 
 
     if(BucketFile.is_open()){
 
         BucketFile.seekg(0, ios::beg);
         BucketFile.write((char*)&ProfundidadLocal, sizeof(int));
+        BucketFile.seekg(4, ios::beg);
         BucketFile.write((char*)&tamanio, sizeof(int));
     }
 
     BucketFile.close();
+
 
 }
 
@@ -74,6 +70,6 @@ public:
     int GetTamanio(){
        return this->tamanio;
    }
-
+    
 };
 
